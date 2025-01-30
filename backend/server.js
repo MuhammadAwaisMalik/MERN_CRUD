058ca384a54815ba9ef0821d915a2b95c12c4b9a
Muhammad Awais <mawaismalik520@gmail.com>
2025-01-30 17:51:51 +0500

    "Update origin in CORS configuration to remove trailing slash"

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://mern-crud-peach.vercel.app/",
+    origin: "https://mern-crud-peach.vercel.app",
     credentials: true,
   })
 );
